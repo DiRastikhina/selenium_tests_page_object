dcea08702aab2070cf40b0a2fbe4155cc55b32fd
DiRastikhina <dianalisheva@gmail.com>
2022-07-20 23:21:50 +0300

    checking login link and page

--- test_main_page.py
+++ test_main_page.py
@@ -6,5 +6,6 @@ def test_guest_can_go_to_login_page(browser):
     #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer "
     page = MainPage(browser,link)
     page.open()
-    page.should_be_login_link()
+    login_page = page.go_to_login_page()
+    login_page.should_be_login_page()
 
