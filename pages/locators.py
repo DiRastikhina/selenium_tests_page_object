25c654c0a385e6fef6f9e629493a4e454752c06c
DiRastikhina <dianalisheva@gmail.com>
2022-07-20 23:26:05 +0300

    fixed mistakes in loginlocators

--- pages/locators.py
+++ pages/locators.py
@@ -6,5 +6,5 @@ class MainPageLocators():
     #LOGIN_LINK = (By.CSS_SELECTOR, "#registration_link")
     
 class LoginPageLocators():
-    LOGIN_FORM = (By.ID, "#login_form")
-    REGISTER_FORM = (By.ID, "#register_form")
+    LOGIN_FORM = (By.ID, "login_form")
+    REGISTER_FORM = (By.ID, "register_form")
