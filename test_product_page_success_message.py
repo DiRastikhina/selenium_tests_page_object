423bbc9cbe5b06f06fd8519d65d0869de3acab79
DiRastikhina <dianalisheva@gmail.com>
2022-07-21 16:06:34 +0300

    вынесли проверку логина в базовый класс

--- test_product_page_success_message.py
+++ test_product_page_success_message.py
@@ -1,6 +1,7 @@
 from .pages.product_page import ProductPage
 import pytest
 
+@pytest.mark.skip
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser,link)
@@ -8,13 +9,15 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page.add_product_into_basket()
     page.solve_quiz_and_get_code()
     page.should_not_be_success_message()
-    
+
+@pytest.mark.skip    
 def test_guest_cant_see_success_message(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser,link)
     page.open()
     page.should_not_be_success_message()
-    
+
+@pytest.mark.skip    
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser,link)
@@ -22,3 +25,15 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.add_product_into_basket()
     page.solve_quiz_and_get_code()
     page.success_message_should_disappear()
+    
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+    
+def test_guest_can_go_to_login_page_from_product_page (browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
